96aceb01f774d47a33c8ed053e0a936b9f38140a
Lucas Zacarias <lucas21111999@gmail.com>
2022-11-12 15:11:54 -0300

    tests of sort character list added

--- shared/src/commonMain/kotlin/com/example/tp_taller_programacion/data/Character.kt
+++ shared/src/commonMain/kotlin/com/example/tp_taller_programacion/data/Character.kt
@@ -3,7 +3,7 @@ package com.example.tp_taller_programacion.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Character(
+data class Character(
     val id: Long,
     val name: String,
     val description: String,
--- shared/src/commonTest/kotlin/domain/SortCharactersTest.kt
+++ shared/src/commonTest/kotlin/domain/SortCharactersTest.kt
@@ -7,15 +7,15 @@ import kotlin.test.*
 class SortCharactersTest {
 
     @Test
-    fun sort_characters_with_description_in_alphabetical_order_by_name(){
+    fun sort_characters_with_description_in_ascending_order_by_id() {
         //Given
         val characterUnsorted = listOf(
-            Character(1L, "Iron Man", "Tony Stark the red and yellow hero", "url"),
-            Character(2L, "Captain America", "", "url"),
-            Character(3L, "Hulk", "The big green hero", "url"),
-            Character(4L, "Thor", "God of Thunder", "url"),
             Character(5L, "Black Widow", "Shield's best agent", "url"),
-            Character(6L, "Hawkeye", "Best equipped hero", "url")
+            Character(2L, "Captain America", "The first super soldier", "url"),
+            Character(6L, "Hawkeye", "Best equipped hero", "url"),
+            Character(3L, "Hulk", "The big green hero", "url"),
+            Character(1L, "Iron Man", "Tony Stark the red and yellow hero", "url"),
+            Character(4L, "Thor", "God of Thunder", "url")
         )
 
         //When
@@ -23,15 +23,72 @@ class SortCharactersTest {
 
         //Then
         val expectedCharacterSortedList = listOf(
-            Character(5L, "Black Widow", "Shield's best agent", "url"),
-            Character(2L, "Captain America", "", "url"),
-            Character(6L, "Hawkeye", "Best equipped hero", "url"),
+            Character(1L, "Iron Man", "Tony Stark the red and yellow hero", "url"),
+            Character(2L, "Captain America", "The first super soldier", "url"),
             Character(3L, "Hulk", "The big green hero", "url"),
+            Character(4L, "Thor", "God of Thunder", "url"),
+            Character(5L, "Black Widow", "Shield's best agent", "url"),
+            Character(6L, "Hawkeye", "Best equipped hero", "url")
+        )
+
+        assertEquals(expected = expectedCharacterSortedList, actual = charactersSorted)
+
+    }
+
+    @Test
+    fun sort_characters_without_description_in_descending_order_by_id() {
+        //Given
+        val characterUnsorted = listOf(
+            Character(5L, "Black Widow", "", "url"),
+            Character(2L, "Captain America", "", "url"),
+            Character(6L, "Hawkeye", "", "url"),
+            Character(3L, "Hulk", "", "url"),
+            Character(1L, "Iron Man", "", "url"),
+            Character(4L, "Thor", "", "url")
+        )
+
+        //When
+        val charactersSorted = CharactersService().sortCharacters(characterUnsorted)
+
+        //Then
+        val expectedCharacterSortedList = listOf(
+            Character(6L, "Hawkeye", "", "url"),
+            Character(5L, "Black Widow", "", "url"),
+            Character(4L, "Thor", "", "url"),
+            Character(3L, "Hulk", "", "url"),
+            Character(2L, "Captain America", "", "url"),
+            Character(1L, "Iron Man", "", "url")
+        )
+
+        assertEquals(expected = expectedCharacterSortedList, actual = charactersSorted)
+    }
+
+    @Test
+    fun sort_characters_with_description_and_without_description_by_id() {
+        //Given
+        val characterUnsorted = listOf(
+            Character(5L, "Black Widow", "", "url"),
+            Character(2L, "Captain America", "The first super soldier", "url"),
+            Character(6L, "Hawkeye", "", "url"),
+            Character(3L, "Hulk", "", "url"),
             Character(1L, "Iron Man", "Tony Stark the red and yellow hero", "url"),
             Character(4L, "Thor", "God of Thunder", "url")
         )
 
+        //When
+        val charactersSorted = CharactersService().sortCharacters(characterUnsorted)
 
+        //Then
+        val expectedCharacterSortedList = listOf(
+            Character(1L, "Iron Man", "Tony Stark the red and yellow hero", "url"),
+            Character(2L, "Captain America", "The first super soldier", "url"),
+            Character(4L, "Thor", "God of Thunder", "url"),
+            Character(6L, "Hawkeye", "", "url"),
+            Character(5L, "Black Widow", "", "url"),
+            Character(3L, "Hulk", "", "url")
+        )
+
+        assertEquals(expected = expectedCharacterSortedList, actual = charactersSorted)
     }
 
 }
